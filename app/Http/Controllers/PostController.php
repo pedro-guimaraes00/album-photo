b936147fc92bf996ec33ff35fd90196dcc0e3fe0
pedro-guimaraes00 <pedrogmendes00@gmail.com>
2020-07-10 12:32:12 -0300

    implementa método store na PostController e cria sua rota

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,7 +36,13 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $post = new Post();
+        $post->email = $request->input('email');
+        $post->mensagem = $request->input('mensagem');
+        $post->arquivo = '';//$request->input('arquivo');
+
+        $post->save();
+        return redirect('/');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PostController@index');
+Route::post('/', 'PostController@store');
